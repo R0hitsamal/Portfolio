f3db3b772ed01dd0db39a5f76fe3a82ca111c79d
Rohit Kumar Samal <139374324+R0hitsamal@users.noreply.github.com>
2024-12-25 18:08:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ createRoot(document.getElementById("root")).render(
         <Router>
           <Routes>
           <Route
-              path="#"
+              path="/"
               element={
                 <>
                   <Homepage />
